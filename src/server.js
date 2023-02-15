f1d8c556ef59fba856b07029da57085421df00a8
Joseph Dunne (RIT Student) <jmd1494@rit.edu>
2023-02-15 14:57:19 -0500

    Response code finished, working on communicating between client and server

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const url = require('url');
 
 const pageHandler = require('./pageResponses.js');
+const userHandler = require('./userResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -9,29 +10,29 @@ const urlStruct = {
   GET: {
     '/': pageHandler.getIndex,
     '/style.css': pageHandler.getStyle,
-    '/getUsers': '',
-    //'/notReal': '',
+    '/getUsers': userHandler.getUsers,
+    '/notReal': userHandler.notFound,
   },
   HEAD: {
-    '/getUsers': '',
-    //'/notReal': '',
+    '/getUsers': userHandler.getUsersHead,
+    '/notReal': userHandler.notFoundHead,
   },
   POST: {
-    //'/AddUser': '',
+    '/AddUser': userHandler.addUser,
   },
 };
 
 const onRequest = (request, response) => {
   const parsedURL = url.parse(request.url);
 
-  if (!urlStruct[request.method][parsedURL.pathname]) {
-    //return urlStruct[request.method]['/notReal'](request, response);
+  if (!urlStruct[request.method]) {
+    return urlStruct.HEAD['/notReal'](request, response);
   }
 
   if (urlStruct[request.method][parsedURL.pathname]) {
     return urlStruct[request.method][parsedURL.pathname](request, response);
   }
-  //return urlStruct[request.method]['/notReal'](request, response);
+  return urlStruct[request.method]['/notReal'](request, response);
 };
 
 http.createServer(onRequest).listen(port, () => {
--- src/userResponses.js
+++ src/userResponses.js
@@ -1,3 +1,68 @@
-const getUser = (request, response) => {
+const users = {};
 
-}
+const getUsers = (request, response) => {
+  // Return user list as json object
+  response.writeHead(200, { 'Content-Type': 'application/json' });
+  response.write(users);
+  response.end();
+};
+
+const getUsersHead = (request, response) => {
+  response.writeHead(200, users);
+  response.end();
+};
+
+const addUser = (request, response, newUser) => {
+  // If missing fields, return error
+  const responseMessage = {
+    message: 'Name and age required',
+  };
+  if (!newUser.name || !newUser.age) {
+    responseMessage.id = 'missingParams';
+    return addUserResponse(request, response, 400, responseMessage);
+  }
+  // Check list of current users
+  // If current user already exists, update their age
+  if (users[newUser.name]) {
+    users[newUser.name].age = newUser.age;
+    responseMessage.message = 'User age updated';
+    return addUserResponse(request, response, 204, responseMessage);
+  }
+  // If user does not exist, create new user
+  users[newUser.name] = {
+    name: newUser.name,
+    age: newUser.age,
+  };
+  responseMessage.message = 'New user created';
+  return addUserResponse(request, response, 201, responseMessage);
+};
+
+// Function for returning addUser response
+const addUserResponse = (request, response, status, message) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify(message));
+  response.end();
+};
+
+// returns error message if page is not found
+const notFound = (request, response) => {
+  response.writeHead(404, { 'Content-Type': 'application/json' });
+  errorMessage = {
+    message: 'The page you are looking for was not found',
+    id: 'notFound',
+  };
+  response.write(JSON.stringify(errorMessage));
+  response.end();
+};
+
+// returns error message meta data if page is not found
+const notFoundHead = (request, response) => {
+  response.writeHead(404, { 'Content-Type': 'application/json' });
+  response.end();
+};
+
+module.exports.getUsers = getUsers;
+module.exports.getUsersHead = getUsersHead;
+module.exports.addUser = addUser;
+module.exports.notFound = notFound;
+module.exports.notFoundHead = notFoundHead;
